5cb57c18c6e618da4968af78bcca642477aa554c
Shibily <shibilyp06@gmail.com>
2024-01-01 18:46:21 +0530

    smaller update

--- controllers/userController.js
+++ controllers/userController.js
@@ -73,6 +73,7 @@ let object = {
     //   res.redirect("/signup"); // Redirect in case of an error
     // }
   },
+  
   postLogin: async (req, res) => {
     let { email, password } = req.body;
     const existingUser = await userModel.findOne({ email: email });
